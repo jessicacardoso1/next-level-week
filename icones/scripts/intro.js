296429b3ef11f05095445175d7cc200c8b12cd1c
Jéssica Cardoso <jessica.cardosor@gmail.com>
2020-06-09 09:34:08 -0300

    Anotaçoes.

---
+++ icones/scripts/intro.js
@@ -0,0 +1,38 @@
+// coments  
+// document.write("Hello")
+
+//variaveis, tipo de dados
+// let myvar
+// const myconst
+// myvar ="Hello"
+// const = "oi"
+
+// document.write(myvar)
+// document.write(myvar)
+// document.write(myvar)
+
+const pessoa = {
+    altura: "1,11",
+    idade: 11,
+    solterio: true
+    correr(){
+      return "run Forest"
+    }
+  }
+  
+  document.write(pessoa.correr())
+  
+  //Array-Vetores
+  
+  const colecaoDeBolinhas =["blue", "green", 1, {name: "My name"}]
+  
+  document.write(colecaoDeBolinhas[0])
+  
+  document.write(colecaoDeBolinhas[3].name)
+  
+  //funcoes 
+  function sayMyName(nome){
+    document.write()
+  }
+  
+  sayMyName("Douglas")
